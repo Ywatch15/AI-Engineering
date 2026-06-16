ee96911bc54c6e38647994576306d33b8749fcba
Ywatch15 <sundram.22460@knit.ac.in>
2026-06-16 21:15:52 +0530

    Learnt string formatting

---
+++ python/Core Py/06_stringFormatting.py
@@ -0,0 +1,11 @@
+def greeting(name):
+    return "Hello, {}!".format(name)
+
+
+print(greeting("Alice"))
+
+
+def welcome(s1,s2,s3):
+    return "Welcome {}, {} and {}".format(s1,s2,s3)
+
+print(welcome("Alice","Bob","Charlie"))
