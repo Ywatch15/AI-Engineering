1f88577aad94d812327e60b681560df6cbdeedce
Ywatch15 <sundram.22460@knit.ac.in>
2026-06-16 20:55:29 +0530

    learnt how to implement maps in py

---
+++ python/Core Py/03_mapFunctions.py
@@ -0,0 +1,12 @@
+def even_odd(n):
+    if n%2==0:
+        return "The given no {} is even".format(n)
+    else:
+        return "The given no {} is odd".format(n)
+
+
+lst=[1,2,3,4,5,6,7,8,89,21,34,56,78,90]
+
+result = list(map(even_odd, lst)) #map is a function that takes a function and an iterable and applies the function to each element of the iterable and returns a map object
+print(result)
+print(endl)
